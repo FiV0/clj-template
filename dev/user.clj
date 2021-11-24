7fe5bc8f616e8bee8177350ed09617d80e92f89c
FiV0 <finn.volkel@gmail.com>
2021-11-24 12:22:56 +0100

    Add tools.namespace by default

--- dev/user.clj
+++ dev/user.clj
@@ -1,1 +1,10 @@
-(ns user)
+(ns user
+  (:require [clojure.java.io :as io]
+            [clojure.tools.namespace.repl :as repl]))
+
+(comment
+  (repl/set-refresh-dirs (io/file "src") (io/file "dev"))
+  (repl/refresh)
+  (repl/clear)
+
+  )
