f69a206c018ee7ff3eb81f1a2aa1e831f778a09e
FiVo <finn.volkel@gmail.com>
2022-11-10 16:02:40 +0100

    datasplash-test

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
-# {project}
+# datasplash test
 
-Executing the program can either be done via
+compile it
 ```
-clj -M -m scratch :arg1 :arg2
-```
-or by compiling a jar via
-```
-clj -T:build clean
 clj -T:build jar
 ```
 and executing it via
 ```
-java -jar target/lib-0.1.4.jar :arg1 :arg2
+java -jar target/lib-0.1.4.jar
 ```
 ## License
--- src/scratch.clj
+++ src/scratch.clj
@@ -1,5 +1,21 @@
 (ns scratch
+  (:require [datasplash.api :as ds])
   (:gen-class))
 
-(defn -main [& args]
-  (println "Hello from Clojure!!! " args))
+(def local-pipeline {:runner "DirectRunner"})
+
+(defn alloc-price-coercion [alloc]
+  (-> alloc
+      (update :suppliers (fn [suppliers] (mapv #(update % :price bigdec) suppliers)))
+      (update-in [:allocation :preferred] (fn [allocs] (mapv #(update % :price bigdec) allocs)))
+      (update-in [:allocation :backup] (fn [allocs] (mapv #(update % :price bigdec) allocs)))
+      (update-in [:allocation :stock] (fn [allocs] (mapv #(update % :price bigdec) allocs)))
+      (update-in [:allocation :closed] (fn [allocs] (mapv #(update % :price bigdec) allocs)))))
+
+(defn -main
+  []
+  (let [pipeline (ds/make-pipeline local-pipeline)
+        data (ds/generate-input [{:suppliers  [{:price 1}] :allocation {:preferred [{:price 2}]}}] pipeline)]
+    (->> (ds/map alloc-price-coercion data)
+         (ds/write-edn-file "out.edn"))
+    (ds/run-pipeline pipeline)))
